62d6fec14b0144cd3e6b193a794a328087aa3de4
Denis <denisjosephjr@gmail.com>
2026-06-23 03:47:27 -0400

    Made progress on the movement method in player.py

--- player.py
+++ player.py
@@ -5,19 +5,21 @@ import math
 class Player():
     def __init__(self, game):
         self.game = game
-        self.x, self.y = PLAYER_POS # i never seen unpacking in a constructor, but it makes sense.
+        self.x, self.y = PLAYER_POS 
         self.angle = PLAYER_ANGLE
     
-    def movement(self): # There is a diagram explaining the logic behind the trig functions in this folder (player_coordinate_formulas.png)
-        sin_a = math.sin(self.angle)
+    def movement(self): # There is a diagram explaining the logic behind the trig
+                        # functions in this folder (player_coordinate_formulas.png)
+        sin_a = math.sin(self.angle) # sin_a is sin(alpha) 
         cos_a = math.cos(self.angle)
         dx, dy = 0, 0
         speed = PLAYER_SPEED * self.game.delta_time
         speed_sin = speed * sin_a
         speed_cos = speed * cos_a
 
-        keys = pygame.key.get_pressed() # a function that can detect the keys that are pressed 
-        if keys[pygame.K_w]: # This is in
+        keys = pygame.key.get_pressed() # List of keys that are currently getting
+                                        # pressed.
+        if keys[pygame.K_w]: 
             dx += speed_cos
             dy += speed_sin
         if keys[pygame.K_s]:
@@ -35,14 +37,15 @@ class Player():
 
         if keys[pygame.K_LEFT]:
             self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
-
+        if keys[pygame.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
 
 
     def update(self):
         self.movement()
     
-    @property  # Never seen this notation before. Let's see.. It is called a decorator and modifies function behavior.
-    def pos(self): # I need to see more examples. Apparently "@property" is a built in decorator that comes with Python
+    @property # This is called a decorator and modifies function behavior.
+    def pos(self): # "@property" is a built in decorator that comes with Python
         return self.x, self.y
 
     @property
